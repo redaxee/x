6f9cacaa9284c10d2d0d285387bb1be3c17b13fd
redaxee <47358833+redaxee@users.noreply.github.com>
2019-08-02 15:07:36 +0700

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,121 @@
+sleep 1
+clear
+echo
+echo
+echo $pur"==================="$cy" ========================================"
+echo $i"XX"$pu"  AUTHOR       :  MR-JACK  "$i"                          XX"
+echo $i"XX"$pu"  Blogger      :  Blog-RedAxee.blogspot.com "$i"         XX"
+echo $i"XX"$pu"  WhatsApp     :  082219449227 "$i"                      XX"
+echo $i"XX"$pu"  Youtube      :  Feri                      "$i"         XX"
+echo $pur"==================="$cy" ========================================="
+echo
+echo $cy"MENU MR-JACK:"
+echo $ku"["$me"1"$ku"]"$i"Spam Call"
+echo $ku"["$me"2"$ku"]"$i"Spam Gmail"
+echo $ku"["$me"3"$ku"]"$i"Brutal Sms"
+echo $ku"["$me"4"$ku"]"$i"Melacak Orang"
+echo $ku"["$me"5"$ku"]"$i"HACK CCTV"
+echo $ku"["$me"6"$ku"]"$i"HACK FACEBOOK TARGET"
+echo $ku"["$me"7"$ku"]"$i"Mencari Kelemahan WEB (RED_HAWK)"
+echo $i"["$me"0"$i"]"$ku" EXIT"
+echo
+echo $pur"["$pur"Masukkan pilihan anda"$pur"]"
+read -p">> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pkg install php -y
+git clone https://github.com/ClayHackerTeam/311
+cd 311
+php SpamCall.php
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install requests
+git clone https://github.com/BlackCoderCrush/BulkMailSpam
+cd BulkMailSpam
+python2 BulkMailSpam.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install requests
+https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install requests
+git clone https://github.com/thelinuxchoice/locator
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install requests
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs 
+python2 scan.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install requests
+https://github.com/redaxee/crkfb
+cd crkfb 
+python2 fb.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "MRX.pen"|lolcat
+sleep 5
+git clone https://github.com/ClayHackerTeam/webdav
+cd webdav
+sh webdav.sh
+fi
+
+if [ $pil = 0 ]
+then
+clear
+figlet -f slant "W A I T"|Ulolcat
+sleep 2
+echo $cy"Terima Kasih sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
+sleep 2
+echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
+sleep 3
+echo $ku"Facebook :"$i" fb.com/redaxeee"
+echo $ku"YouTube  :"$i" Feri"
+echo $ku"WhatsApp :"$i" 082219449227"
+sleep 3
+echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
+sleep 3
+echo $pur"SUBSCRIBE MY CHANNEL { Feri }"
+exit
+fi
