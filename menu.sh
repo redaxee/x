d5507baf3387832fcfc0e3795ee4e89fc406eccd
redaxee <47358833+redaxee@users.noreply.github.com>
2019-08-02 18:26:59 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,3 +1,16 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+echo
+python2 xz87.py
+echo
+clear
 sleep 1
 clear
 echo
