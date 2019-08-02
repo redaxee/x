564b35d1a45aa8ead4f385d649e39400a3a2401c
redaxee <47358833+redaxee@users.noreply.github.com>
2019-08-02 15:38:03 +0700

    Update x.py

--- x.py
+++ x.py
@@ -1,62 +1,41 @@
-﻿from getpass import getpass
-
+from getpass import getpass
 import os
-
 import time
 
-
-	def menu():
+def menu():
       while True:
            print("")
-           
-	os.system("clear")
-           
-print('\033[1;36;40m<===============[ MR-JACK ] ===============>')
-           
-print('')
+           os.system("clear")
+           print('\033[1;36;40m<===============[ MR-JACK ] ===============>')
+           print('')
            os.system('date | lolcat')
            print("\033[1;93m")
-           
-print(" \033[1;92m   Silahkan Login Terlebih dahulu')")
-           p
-rint("\033[1;93m")
-           
-print("  <===============[ MR-JACK ] ===============>")
-           
-print("")
+           print(" \033[1;92m   Silahkan Login Eaa Sayang')")
+           print("\033[1;93m")
+           print('\033[1;36;40m<===============[ MR-JACK ] ===============>')
+           print("")
            try:
                 x = str(input('\033[1;92mUsername \033[1;93m: '))
-                
-print("")
+                print("")
                 e = getpass('\033[1;92mPassword \033[1;93m: ')
-                
-print ("")
-                if x=="Mr-Jack" and e=="RedAxee":
-                   
-print('Login Sukses Mohon Tunggu Sebentar...')
-                   
-time.sleep(1)
-                   
-os.system('clear')
-                   
-print(' [+]=> Sedang Proses...;')
-sh menu.sh                   
-os.system('xdg-open https://www.youtube.com/channel/UCuMk-x9YpT1ctiYh3HpGvRw')                   
-print('\033[1;92m ======================================================== ')
-                   
-print("")
+                print ("")
+                if x=="Username" and e=="Password":
+                   print('Login Sukses Mohon Tunggu Sebentar...')
+                   time.sleep(1)
+                   os.system('clear')
+                   print('')
+                   os.system('figlet ' + x + ' | lolcat')
+                   print('\033[1;36;40m<===============[ MR-JACK ] ===============>')
+                   print("")
                    break
                 else:
                       print("")
                       print("")
                       print("")
                       print("")
-                      
-print("\033[1;91m     Password/Username  Salah")
-                      
-time.sleep(2)
-                      
-print("")
+                      print("\033[1;91m     Password/Usr Name Salah")
+                      time.sleep(2)
+                      print("")
            except Exception:
                       
                       print("")
@@ -64,19 +43,15 @@ print("")
                       print("")
                       print("")
                       print("")
-                      
-print("\033[1;91m     Password/Username  Salah")
-                      
-time.sleep(2)
+                      print("\033[1;91m     Password/Usr Name Salah")
+                      time.sleep(2)
            except KeyboardInterrupt:
                       print("")
-                      
-os.system('killall -9 com.termux')
+                      os.system('killall -9 com.termux')
                       print("")
                       print("")
                       print("")
                       print("")
-                     
-print("\033[1;91m     Password/Username Salah")
+                      print("\033[1;91m     Password/Usr Name Salah")
                       time.sleep(2)
 menu()
