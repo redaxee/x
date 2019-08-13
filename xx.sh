f834d06d06879fa3cc733b07e0fcd98653b2c331
redaxee <47358833+redaxee@users.noreply.github.com>
2019-08-13 16:10:17 +0700

    Update xx.sh

--- xx.sh
+++ xx.sh
@@ -28,7 +28,7 @@ echo $i"["$me"6"$ku"]"$i"CRACK FACEBOOK TARGET"
 echo $i"["$me"7"$ku"]"$i"Mencari Kelemahan WEB (RED_HAWK)"
 echo $i"["$me"8"$ku"]"$i"SMS GRATIS"
 echo $i"["$me"9"$ku"]"$i"PAKET TELKOMSEL 15GB 75K"
-echo $i"["$ku"10"$ku"]$i"HACK @WIFI.ID"
+echo $i"["$ku"10"$ku"]"$i"HACK @WIFI.ID"
 echo $ku"["$me"0"$me"]"$ku" EXIT"
 echo
 echo $cya"["$bi"Masukkan pilihan anda"$bi"]"
